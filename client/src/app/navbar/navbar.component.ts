3f08eef8621453d0ceca5f07153943507346db6f
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-11-21 23:19:06 -0600

    configure routing in navbar

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabMenuModule } from 'primeng/tabmenu';
-
-interface TabItem {
-  label: string;
-  icon: string;
-}
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -18,15 +15,19 @@ interface TabItem {
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  items: TabItem[];
+  items: MenuItem[];
 
-  constructor() {
+  constructor(private router: Router) {
     this.items = [
-      { label: 'Mapa en Tiempo Real', icon: 'pi pi-fw pi-map' },
-      { label: 'Rutas de la Empresa', icon: 'pi pi-fw pi-arrows-h' },
-      { label: 'Camiones de la Empresa', icon: 'pi pi-fw pi-truck' },
-      { label: 'Análisis de Datos', icon: 'pi pi-fw pi-chart-bar' },
-      { label: 'Cerrar sesión', icon: 'pi pi-fw pi-power-off' }
+      { label: 'Mapa en Tiempo Real', icon: 'pi pi-fw pi-map', command: () => { this.router.navigate(['/map']) } },
+      { label: 'Rutas de la Empresa', icon: 'pi pi-fw pi-arrows-h', command: () => { this.router.navigate(['/routes']) } },
+      { label: 'Camiones de la Empresa', icon: 'pi pi-fw pi-truck', command: () => { this.router.navigate(['/trucks']) } },
+      { label: 'Análisis de Datos', icon: 'pi pi-fw pi-chart-bar', command: () => { this.router.navigate(['/data']) } },
+      { label: 'Cerrar sesión', icon: 'pi pi-fw pi-power-off', command: () => {
+        localStorage.removeItem('accessToken');
+        this.router.navigate(['/map']);
+       }
+      }
     ];
   }
 }
