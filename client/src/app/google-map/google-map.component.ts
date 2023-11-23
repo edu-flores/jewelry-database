10eb465e6b6bb7cb71839617c3e46ca45f616bbf
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-11-22 23:38:17 -0600

    set locations on map

--- client/src/app/google-map/google-map.component.ts
+++ client/src/app/google-map/google-map.component.ts
@@ -16,9 +16,18 @@ export class GoogleMapComponent implements AfterViewInit {
 
   @ViewChild('mapContainer', { static: false }) gmap!: ElementRef;
   map: google.maps.Map | undefined;
+  truckMarkers: { [key: number]: google.maps.Marker } = {};
+  airCircles: google.maps.Circle[] = [];
+  timer: any;
 
-  // Get truck's locations and air quality data from the GPS service
-  ngOnInit() {
+  ngAfterViewInit() {
+    // Set up the map
+    this.map = new google.maps.Map(this.gmap.nativeElement, {
+      center: { lat: 25.687, lng: -100.318 },
+      zoom: 11
+    });
+
+    // Call GPS API service
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
     });
@@ -26,6 +35,7 @@ export class GoogleMapComponent implements AfterViewInit {
     this.http.get<any>('http://localhost:5002/get-locations', { headers }).subscribe(
       (response) => {
         console.log('Data from API:', response);
+        this.placeLocations(response);
       },
       (error) => {
         console.error('Error fetching data:', error);
@@ -33,10 +43,65 @@ export class GoogleMapComponent implements AfterViewInit {
     );
   }
 
-  ngAfterViewInit() {
-    this.map = new google.maps.Map(this.gmap.nativeElement, {
-      center: { lat: 25.687, lng: -100.318 },
-      zoom: 11
+  // Place locations on the map
+  private placeLocations = (response: any) => {
+    // Company trucks
+    response.trucks.forEach((truck: any) => {
+      const marker = new google.maps.Marker({
+        position: { lat: parseFloat(truck.latitude), lng: parseFloat(truck.longitude) },
+        map: this.map,
+        title: `Ubicación del Camión: ${truck.name}`
+      });
+      const infowindow = new google.maps.InfoWindow({
+        content: `
+          <div style="font-size: 14px; color: #333;">
+            <strong>Camión:</strong> ${truck.name} ICA<br>
+          </div>
+        `
+      });
+      marker.addListener("click", () => {
+        infowindow.open(this.map, marker);
+      });
+      this.truckMarkers[truck.id] = marker;
     });
+
+    // Air quality
+    response.air.forEach((record: any) => {
+      const circle = new google.maps.Circle({
+        map: this.map,
+        fillColor: this.getColorForQuality(record.quality),
+        fillOpacity: 0.15,
+        strokeOpacity: 0,
+        center: { lat: parseFloat(record.latitude), lng: parseFloat(record.longitude) },
+        radius: 5000
+      });
+      const infowindow = new google.maps.InfoWindow({
+        content: `
+          <div style="font-size: 14px; color: #333;">
+            <strong>Quality:</strong> ${record.quality} ICA<br>
+            <strong>Has Contaminants:</strong> ${record.contaminants ? "Yes" : "No"}
+          </div>
+        `
+      });
+      google.maps.event.addListener(circle, "click", event => {
+        infowindow.setPosition(event.latLng);
+        infowindow.open(this.map);
+      });
+      this.airCircles.push(circle);
+    })
+  }
+
+  // Function to get color based on quality
+  private getColorForQuality = (quality: number) => {
+    const hue = (1 - quality / 500) * 120;
+    return `hsl(${hue}, 100%, 50%)`;
+  }
+
+  // Function to remove all circles from the map
+  private removeCircles = () => {
+    this.airCircles.forEach(circle => {
+      circle.setMap(null);
+    });
+    this.airCircles.length = 0;
   }
 }
