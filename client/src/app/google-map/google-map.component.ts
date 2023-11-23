aa88572b184731d8cc9a44ecddf434c431c97ff1
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-11-22 23:16:53 -0600

    get data from gps api

--- client/src/app/google-map/google-map.component.ts
+++ client/src/app/google-map/google-map.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-google-map',
@@ -11,8 +12,26 @@ import { CommonModule } from '@angular/common';
   styleUrl: './google-map.component.scss'
 })
 export class GoogleMapComponent implements AfterViewInit {
+  constructor(private http: HttpClient) { }
+
   @ViewChild('mapContainer', { static: false }) gmap!: ElementRef;
   map: google.maps.Map | undefined;
+
+  // Get truck's locations and air quality data from the GPS service
+  ngOnInit() {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    });
+
+    this.http.get<any>('http://localhost:5002/get-locations', { headers }).subscribe(
+      (response) => {
+        console.log('Data from API:', response);
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 
   ngAfterViewInit() {
     this.map = new google.maps.Map(this.gmap.nativeElement, {
